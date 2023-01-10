47f9dd6433c12019a07be136235cbe3e456f86c9
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-01-10 17:13:50 -0600

    Extracts specific fields from original file and writes it into a new one

---
+++ anonymize_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+with open('people-100.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    #should I anonymize just the email field here?
+
+    with open('new-people-100.csv', 'w') as new_file:
+        fieldnames = ['First Name', 'Last Name', 'Email', 'Job Title']
+
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, extrasaction='ignore', delimiter='\t')
+        csv_writer.writeheader() # writes the header from the original CSV into the new one
+
+        for line in csv_reader:
+            del line['Email'] #deletes the email field before writing to the new file
+            csv_writer.writerow(line)
