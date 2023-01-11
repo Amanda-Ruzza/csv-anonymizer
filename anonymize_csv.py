863df31dd816545726a23e6caf939fcfe43279ad
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-01-10 21:04:19 -0600

    Wrote the 'Anonymizer' on a test file

--- anonymize_csv.py
+++ anonymize_csv.py
@@ -6,11 +6,11 @@ with open('people-100.csv', 'r') as csv_file:
     #should I anonymize just the email field here?
 
     with open('new-people-100.csv', 'w') as new_file:
-        fieldnames = ['First Name', 'Last Name', 'Email', 'Job Title']
+        fieldnames = ['Index', 'First Name', 'Last Name', 'Email', 'Job Title']
 
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, extrasaction='ignore', delimiter='\t')
         csv_writer.writeheader() # writes the header from the original CSV into the new one
 
         for line in csv_reader:
-            del line['Email'] #deletes the email field before writing to the new file
+            del line['First Name'] #deletes the 'First Name' field before writing to the new file
             csv_writer.writerow(line)
---
+++ test_anonymizer.py
@@ -0,0 +1,15 @@
+# Generating an anonymized ID from an 'Account ID'
+
+import hashlib
+
+account_id=float(4572) # '4572' is the Customer Account ID number
+HASH_SALT="keij3ka2Hie2lilie1aiwab5oaQuooth"
+salted_value = "{}{}".format(account_id, HASH_SALT) # combines the 'Customer Account ID number' with the 'HASH_SALT'
+
+# Using the Hashlib function to create a random/anonymized account ID
+anonymized_value=str(hashlib.sha1(salted_value.encode("utf-8")).hexdigest())[:12]
+print (f"This is the 'Anonymized Account ID number': ", anonymized_value)
+# print(anonymized_value)
+
+#
+# In order for the 'float' to work (line 5), use the 'Index Column' from the people-100.csv file
