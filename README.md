b9a6b71c621c47f0e3230877f9ad9252dd663336
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-01-12 11:13:30 -0600

    Removed hard coded values, created CLI options

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# csv-anonymizer
-Python script that extracts a column from a CSV file, anonymizes the data, and parses into a new CSV file
+# CSV Anonymizer
+Python script that extracts a column from a CSV file, anonymizes the data from one specific column, and writes into a new CSV file
+
+## Running Instructions:
+
+- The original file needs to be either in the project folder, or a full path
+- The original file needs to have a ``Header`` 
+- To run the file, execute the following command:
+    ```
+    python anonymize_full_script.py -i <inputfilename> -o <outputfilename> -c <columnname>  
+    ```
+ ``-c, or --column`` is the name of the column which needs to be anonymized
+
+
+## Future Improvements:
+- Create a handle that defines weather if the file has a Header
+- Create an exeption handling for when the file is not in the project folder
--- anonymize_full_script.py
+++ anonymize_full_script.py
@@ -1,39 +1,58 @@
+
 import csv
 import hashlib
 import logging
+import sys, getopt
 
 logger = logging.Logger(__name__)
 
 HASH_SALT = "keij3ka2Hie2lilie1aiwab5oaQuooth"
 
+# This the function where we want to entry the script
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    columnname = ''
+    try:
+       opts, args = getopt.getopt(argv[1:],"hi:o:c:",["ifile=","ofile=", "column="])
+    except getopt.GetoptError:
+       print (f"{argv[0]} -i <inputfile> -o <outputfile> -c <columnname>")
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+           print (f"{argv[0]} -i <inputfile> -o <outputfile> -c <columnname>")
+           sys.exit()
+       elif opt in ("-i", "--ifile"):
+           inputfile = arg
+       elif opt in ("-o", "--ofile"):
+           outputfile = arg
+       elif opt in ("-c", "--column"):
+           columnname = arg
+
+    logger.info('Input file is: ', inputfile)
+    parse_file(inputfile, outputfile, columnname)
+    logger.info('Output file is: ', outputfile)
 
 def hash_field(field: str) -> str:
     salted_value = "{}{}".format(field, HASH_SALT)
-
     return str(hashlib.sha1(salted_value.encode("utf-8")).hexdigest())[:12]
 
 
-def main():
-    logger.info("main")
-    # TODO set input file from cli argument
-    # TODO no hardcoding file names
-    with open('people-100.csv', 'r') as csv_file:
+def parse_file(input_file_name, output_file_name, columname):
+    with open(input_file_name, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-
-        # TODO set output file from cli argument
-        # TODO no hardcoding file names
-        with open('new-people-100.csv', 'w') as new_file:
+        with open(output_file_name, 'w') as new_file:
             # TODO have an argument for if the csv has headers or not
             # TODO if the script is using header values which columns should we hash by header use DictReader
             # TODO if the script is not using header values which columns should we hash by column index use reader
-            fieldnames = ['Index', 'First Name', 'Last Name', 'Email', 'Job Title']
+            fieldnames = next(csv_reader)
             csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, extrasaction='ignore', delimiter='\t')
             csv_writer.writeheader() # writes the header from the original CSV into the new one
 
             for line in csv_reader:
-                line["Index"] = hash_field(line["Index"])
+                line[columname] = hash_field(line[columname])
                 csv_writer.writerow(line)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
